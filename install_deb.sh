b59ab777951db7b366ca9ac782a23e32d5cfed3a
Sean McBride <spmcbride1201@gmail.com>
2021-03-02 15:41:31 -0500

    chore: script and README fixes

--- install_deb.sh
+++ install_deb.sh
@@ -12,40 +12,34 @@ if [[ $(git remote get-url origin) != *"/aWsm.git" || $(git rev-parse --show-top
 fi
 shopt -u nocasematch
 
+# Install Build Dependencies
+sudo apt install build-essential --yes
+sudo apt install cmake --yes
+
+# Initialize Wasmception submodule
 # Check to see if ./wasmception directory exists and is not empty.
 # If not assume user didn't initialize submodules
 if [[ ! -d "./wasmception" ]] || [[ -z "$(ls -A wasmception)" ]]; then
   git submodule update --init --recursive
 fi
 
-# Wasmception
-# Install Subversion. Is this actually still needed?
-sudo apt install subversion --yes
-sudo apt install build-essential --yes
-sudo apt install cmake --yes
-
-# Build Wasmception
-# This is super slow. Does something need to get modified in the Makefile?
+# Build Wasmception from Source
 cd wasmception || exit
 make
 cd .. || exit
 
 # Install Rust
-
 if [[ -x "$(command -v rustup)" ]]; then
   rustup update
 else
   curl https://sh.rustup.rs -sSf | bash -s -- -y
 fi
-
 source "$HOME/.cargo/env"
 export PATH="$HOME/.cargo/bin:$PATH"
 
 # Install LLVM build dependencies
 LLVM_VERSION=9
-wget https://apt.llvm.org/llvm.sh
-chmod +x llvm.sh
-sudo ./llvm.sh $LLVM_VERSION
+sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)" bash $LLVM_VERSION
 sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-$LLVM_VERSION 100
 sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-$LLVM_VERSION 100
 sudo update-alternatives --install /usr/bin/llvm-config llvm-config /usr/bin/llvm-config-$LLVM_VERSION 100
