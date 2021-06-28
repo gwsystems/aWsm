c1922afe5b0d772eb8245e424ab63f150f2e12e7
Sean McBride <spmcbride1201@gmail.com>
2021-06-28 15:32:45 -0400

    docs: Cleanup inline comments in sample Makefile

--- code_benches/dummy.cpp
+++ code_benches/dummy.cpp
@@ -1,3 +1,8 @@
+/* This is a C++ wrapper around dummy.c. 
+ * It is provided in order to enable use of -std and other C++ flags that trigger errors when
+ * compiling mixed C and C++ translation units
+ */
+
 extern "C"
 {
 #include "dummy.c"
