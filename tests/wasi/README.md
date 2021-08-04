03639335b3ca53c7dc3b66ed8672bee780be7125
Sean McBride <spmcbride1201@gmail.com>
2021-08-04 10:10:54 -0400

    chore: Add MIT license to node derived tests

--- tests/wasi/README.md
+++ tests/wasi/README.md
@@ -1,7 +1,11 @@
 # WASI Tests
 
-Inspired by the WASI tests in the Node.js project
-https://github.com/nodejs/node/tree/master/test/wasi
+Many of these tests are derived work from the Node.js project
+https://github.com/nodejs/node/tree/d872aaf1cf20d5b6f56a699e2e3a64300e034269/test/wasi
+
+The derived source files are prepended with the appropriate Node.js license information. 
+Tests lacking such license information are original to this project and retain the license
+outlined in the top-level LICENSE file.
 
 ## To run
 
--- tests/wasi/c/write_file.c
+++ tests/wasi/c/write_file.c
@@ -1,3 +1,33 @@
+/* 
+This test was derived from Node.js source code located at the following URL:
+https://github.com/nodejs/node/blob/d872aaf1cf20d5b6f56a699e2e3a64300e034269/test/wasi/c/write_file.c
+
+It retains the the Node.js license as follows:
+
+MIT License
+-----------
+
+Copyright Node.js contributors. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to
+deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+IN THE SOFTWARE.
+*/
+
 #include <assert.h>
 #include <stdio.h>
 #include <string.h>
