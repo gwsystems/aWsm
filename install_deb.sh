e9b805526a2c6d63e32e990b4286c0274241e970
Sean McBride <spmcbride1201@gmail.com>
2021-08-17 18:10:34 -0400

    chore: add wasi-sdk-12

--- install_deb.sh
+++ install_deb.sh
@@ -4,7 +4,7 @@
 
 sudo="sudo"
 if [[ $(whoami) == "root" ]]; then
-  sudo=""
+	sudo=""
 fi
 
 # Exit if not executing script from project root because we use relative paths
@@ -12,8 +12,8 @@ fi
 # directory is case sensitive. Thus, we can end up with either "aWsm" or "awsm."
 shopt -s nocasematch
 if [[ $(git remote get-url origin) != *"/awsm"* || $(git rev-parse --show-toplevel) != $(pwd) ]]; then
-  echo "Install script must be run from project root."
-  exit 1
+	echo "Install script must be run from project root."
+	exit 1
 fi
 shopt -u nocasematch
 
@@ -26,7 +26,7 @@ $sudo apt install lsb-release wget software-properties-common --yes
 # Check to see if ./wasmception directory exists and is not empty.
 # If not assume user didn't initialize submodules
 if [[ ! -d "./wasmception" ]] || [[ -z "$(ls -A wasmception)" ]]; then
-  git submodule update --init --recursive
+	git submodule update --init --recursive
 fi
 
 # Build Wasmception from Source
@@ -36,9 +36,9 @@ cd .. || exit
 
 # Install Rust
 if [[ -x "$(command -v rustup)" ]]; then
-  rustup update
+	rustup update
 else
-  curl https://sh.rustup.rs -sSf | bash -s -- -y
+	curl https://sh.rustup.rs -sSf | bash -s -- -y
 fi
 source "$HOME/.cargo/env"
 export PATH="$HOME/.cargo/bin:$PATH"
@@ -49,17 +49,24 @@ $sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)" bash $LLVM_VERSION
 $sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-$LLVM_VERSION 100
 $sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-$LLVM_VERSION 100
 $sudo update-alternatives --install /usr/bin/llvm-config llvm-config /usr/bin/llvm-config-$LLVM_VERSION 100
-$sudo apt install libc++-dev libc++abi-dev --yes
+sudo apt install libc++-11-dev libc++abi-11-dev --yes
 
 # Build and install aWsm
 cargo build --release
 $sudo cp -t /usr/bin ./target/release/awsm
+
+# Install WASI-SDK 12
+wget https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-12/wasi-sdk-12.0-linux.tar.gz
+tar -xvf wasi-sdk-12.0-linux.tar.gz
+mv wasi-sdk-12.0 wasi-sdk
+rm wasi-sdk-12.0-linux.tar.gz
 
 # Install libuv
 pushd runtime || exit
 wget https://github.com/libuv/libuv/archive/refs/tags/v1.42.0.tar.gz
 tar -xvf v1.42.0.tar.gz
 mv libuv-1.42.0/ libuv
+rm v1.42.0.tar.gz
 pushd libuv || exit
 mkdir -p build
 pushd build || exit
