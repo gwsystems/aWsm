11c83f5383f1c3e1402d5b2bb0cc7bc055a22008
Sean McBride <spmcbride1201@gmail.com>
2021-08-27 16:50:13 -0400

    chore: cleanup config automation and fix Makefiles

--- install_deb.sh
+++ install_deb.sh
@@ -55,39 +55,10 @@ sudo apt install libc++-11-dev libc++abi-11-dev --yes
 cargo build --release
 $sudo cp -t /usr/bin ./target/release/awsm
 
-# Install WASI-SDK 12
-wget https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-12/wasi-sdk-12.0-linux.tar.gz
-tar -xvf wasi-sdk-12.0-linux.tar.gz
-mv wasi-sdk-12.0 wasi-sdk
-rm wasi-sdk-12.0-linux.tar.gz
+# Install WASI-SDK 12 to /opt/wasi-sdk, the path wasi-sdk assumed by default
+wget https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-12/wasi-sdk_12.0_amd64.deb
+dpkg -i wasi-sdk_12.0_amd64.deb
+rm -f wasi-sdk_12.0_amd64.deb
 
-# Install libuv
-pushd runtime || exit
-wget https://github.com/libuv/libuv/archive/refs/tags/v1.42.0.tar.gz
-tar -xvf v1.42.0.tar.gz
-mv libuv-1.42.0/ libuv
-rm v1.42.0.tar.gz
-pushd libuv || exit
-mkdir -p build
-pushd build || exit
-cmake ..
-cmake --build .
-popd || exit
-popd || exit
-popd || exit
-
-# Install uvwasi
-pushd runtime || exit
-wget https://github.com/nodejs/uvwasi/archive/refs/tags/v0.0.11.tar.gz
-tar -xvf v0.0.11.tar.gz
-mv uvwasi-0.0.11 uvwasi
-rm v0.0.11.tar.gz
-pushd uvwasi || exit
-mkdir -p out/cmake
-pushd out/cmake || exit
-cmake ../.. -DBUILD_TESTING=ON
-cmake --build .
-ctest -C Debug --output-on-failure
-popd || exit
-popd || exit
-popd || exit
+# Install libuv and uvwasi
+make -C ./runtime/thirdparty install
