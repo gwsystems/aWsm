c15972d3a229da5cb834052effee9522ed2995fc
Sean McBride <spmcbride1201@gmail.com>
2021-08-27 16:50:13 -0400

    chore: cleanup config automation and fix Makefiles

--- code_benches/example.mk
+++ code_benches/example.mk
@@ -12,35 +12,23 @@ UNAME:=$(shell uname -m)
 ROOT_PATH:=$(shell cd .. && realpath .)
 RUNTIME_PATH:=${ROOT_PATH}/runtime
 
+RUNTIME_CPATH+=${RUNTIME_PATH}/runtime.c
+RUNTIME_CPATH+=${RUNTIME_PATH}/libc/wasi/wasi_main.c
+RUNTIME_CPATH+=${RUNTIME_PATH}/libc/wasi/wasi_backing.c
+RUNTIME_CPATH+=${RUNTIME_PATH}/libc/wasi/wasi_impl_uvwasi.c
+RUNTIME_CPATH+=${RUNTIME_PATH}/libc/env.c
+RUNTIME_CPATH+=${RUNTIME_PATH}/memory/64bit_nix.c
+RUNTIME_CPATH+=${RUNTIME_PATH}/thirdparty/dist/lib/libuv_a.a
+RUNTIME_CPATH+=${RUNTIME_PATH}/thirdparty/dist/lib/libuvwasi_a.a
+
+RUNTIME_INCLUDES=-I../runtime/libc/wasi/include -I../runtime/thirdparty/dist/include
+
+WASMCC=/opt/wasi-sdk/bin/clang
+
 AWSM_CC:=${ROOT_PATH}/target/release/awsm
 
-# Use WASI_SDK if set to true. Wasmception otherwise
-USE_WASI_SDK:=true
-
-WASMCEPTION_PATH:=${ROOT_PATH}/wasmception
-WASMCEPTION_CLANG:=${WASMCEPTION_PATH}/dist/bin/clang
-WASMCEPTION_SYSROOT:=${WASMCEPTION_PATH}/sysroot
-WASMCEPTION_FLAGS:=--target=wasm32-unknown-unknown-wasm -nostartfiles ${OPTFLAGS} --sysroot=${WASMCEPTION_SYSROOT}
-WASMCEPTION_BACKING:=wasmception_backing.c
-
-WASI_SDK_PATH:=${ROOT_PATH}/wasi-sdk
-WASI_SDK_CLANG:=${WASI_SDK_PATH}/bin/clang
-WASI_SDK_SYSROOT:=${WASI_SDK_PATH}/share/wasi-sysroot
-WASI_SDK_FLAGS:=--target=wasm32-wasi -mcpu=mvp -nostartfiles ${OPTFLAGS} --sysroot=${WASI_SDK_SYSROOT}
-WASI_SDK_BACKING:=wasi_sdk_backing.c
-
-ifeq ($(USE_WASI_SDK), true)
-	WASM_CC:=${WASI_SDK_CLANG}
-	WASM_FLAGS:=${WASI_SDK_FLAGS}
-	WASM_BACKING:=${WASI_SDK_BACKING}
-else
-	WASM_CC:=${WASMCEPTION_CLANG}
-	WASM_FLAGS:=${WASMCEPTION_FLAGS}
-	WASM_BACKING:=${WASMCEPTION_BACKING}
-endif
-
 # Excludes stack-size, as this may vary between apps
-WASM_LINKER_FLAGS:=-Wl,--allow-undefined,--no-threads,--stack-first,--no-entry,--export-all,--export=main,--export=dummy
+WASM_LINKER_FLAGS:=-Wl,-z,stack-size=524288,--allow-undefined,--threads=1
 
 app_pid: app_pid_native app_pid_np_us app_pid_np app_pid_bc app_pid_vm
 
@@ -52,72 +40,20 @@ clean: app_pid_clean
 
 # TODO: Directly executing the native binary does not write output to the console as with the
 # various awsm builds. This behavior is observable when building with run.py as well.
-.PHONY: app_pid_native
-app_pid_native:
+./app_pid/bin/app_pid_native:
 	@mkdir -p ./app_pid/bin
 	@${CC} -lm ${OPTFLAGS} -Wall -std=c++11 ./app_pid/*.cpp -o ./app_pid/bin/app_pid_native
 
-.PHONY: app_pid.wasm
-app_pid.wasm: ./dummy.cpp ./app_pid/*.cpp
-ifeq ($(USE_WASI_SDK), true)
-	@echo "Building with WASK-SDK"
-else
-	@echo "Building with Wasmception"
-endif
+./app_pid/bin/app_pid.wasm: ./app_pid/*.cpp
 	@mkdir -p ./app_pid/bin
-	@${WASM_CC} ${WASM_FLAGS} ${WASM_LINKER_FLAGS} -Wl,-z,stack-size=256  -Wall -std=c++11 ./dummy.cpp ./app_pid/*.cpp -o ./app_pid/bin/app_pid.wasm
+	echo "Building with WASK-SDK"
+	mkdir -p ./app_pid/bin
+	${WASMCC} ${OPTFLAGS} ${WASM_LINKER_FLAGS} -Wl,-z,stack-size=256  -Wall -std=c++11 ./app_pid/*.cpp -o ./app_pid/bin/app_pid.wasm
 
-.PHONY: app_pid.bc
-app_pid.bc: app_pid.wasm
+./app_pid/bin/app_pid.bc: ./app_pid/bin/app_pid.wasm
+	mkdir -p ./app_pid/bin
+	${AWSM_CC} ./app_pid/bin/app_pid.wasm -o ./app_pid/bin/app_pid.bc
+
+./app_pid/bin/app_pid_vm: ./app_pid/bin/app_pid.bc
 	@mkdir -p ./app_pid/bin
-	@${AWSM_CC} ./app_pid/bin/app_pid.wasm -o ./app_pid/bin/app_pid.bc
-	
-# Generate LLVM bitcode using the "fast unsafe implementations" option
-.PHONY: app_pid_us.bc
-app_pid_us.bc: app_pid.wasm
-	@mkdir -p ./app_pid/bin
-	@${AWSM_CC} -u ./app_pid/bin/app_pid.wasm -o ./app_pid/bin/app_pid_us.bc
-
-# Run unsafe code using the "No Protection" memory model
-.PHONY: app_pid_np_us
-app_pid_np_us: app_pid_us.bc
-	@${CC} -lm ${OPTFLAGS} \
-	./app_pid/bin/app_pid_us.bc \
-	${RUNTIME_PATH}/runtime.c \
-	${RUNTIME_PATH}/libc/${WASM_BACKING} \
-	${RUNTIME_PATH}/libc/env.c \
-	${RUNTIME_PATH}/memory/no_protection.c \
-	-o ./app_pid/bin/app_pid_np_us
-
-# Run safe code using the "No Protection" memory model
-.PHONY: app_pid_np
-app_pid_np: app_pid.bc
-	@${CC} -lm ${OPTFLAGS} \
-	./app_pid/bin/app_pid.bc \
-	${RUNTIME_PATH}/runtime.c \
-	${RUNTIME_PATH}/libc/${WASM_BACKING} \
-	${RUNTIME_PATH}/libc/env.c \
-	${RUNTIME_PATH}/memory/no_protection.c \
-	-o ./app_pid/bin/app_pid_np
-
-# Run safe code using the "Generic" memory model
-.PHONY: app_pid_bc
-app_pid_bc: app_pid.bc
-	@${CC} -lm ${OPTFLAGS} \
-	./app_pid/bin/app_pid.bc \
-	${RUNTIME_PATH}/runtime.c \
-	${RUNTIME_PATH}/libc/${WASM_BACKING} \
-	${RUNTIME_PATH}/libc/env.c \
-	${RUNTIME_PATH}/memory/generic.c \
-	-o ./app_pid/bin/app_pid_bc
-
-# Run safe code using the "Virtual Memory" memory model
-.PHONY: app_pid_vm
-app_pid_vm: app_pid.bc
-	@${CC} -lm ${OPTFLAGS} \
-	./app_pid/bin/app_pid.bc \
-	${RUNTIME_PATH}/runtime.c \
-	${RUNTIME_PATH}/libc/${WASM_BACKING} \
-	${RUNTIME_PATH}/libc/env.c \
-	${RUNTIME_PATH}/memory/64bit_nix.c \
-	-o ./app_pid/bin/app_pid_vm
+	${CC} -lm -lpthread -ldl ${OPTFLAGS} ${RUNTIME_INCLUDES} ${RUNTIME_CPATH} ./app_pid/bin/app_pid.bc -o ./app_pid/bin/app_pid_vm
