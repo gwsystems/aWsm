5c9afb9a13a6e2d917533b29d4e21b9cbe32dcf1
Sean McBride <spmcbride1201@gmail.com>
2021-09-08 16:36:13 -0400

    docs: Add uvwasi license to wasi_serdes.h

--- runtime/libc/wasi/include/wasi_serdes.h
+++ runtime/libc/wasi/include/wasi_serdes.h
@@ -1,7 +1,36 @@
 #ifndef __WASI_SERDES_H__
 #define __WASI_SERDES_H__
 
-// TODO: Add Node.js license
+/*
+The serialization/deserialization logic in this file is derived from uvwasi source code
+located at the following URL:
+https://github.com/nodejs/uvwasi/blob/7523499546c351a1642442e6ec7bd32bcac76e2c/src/wasi_serdes.c
+
+It retains the the Node.js license as follows:
+
+MIT License
+-----------
+
+Copyright (c) 2019 Colin Ihrig and Contributors
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 
 #include "wasi_spec.h"
 
