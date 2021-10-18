2d5988ee7d71c7d39502e980c218aba268f8032b
Sean McBride <spmcbride1201@gmail.com>
2021-10-18 09:33:40 -0400

    test: Simplify atof test

--- tests/wasi/c/atof.c
+++ tests/wasi/c/atof.c
@@ -1,18 +1,8 @@
-#include <assert.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-int main(int argc, char** argv) {
-    for (int i = 0; i < argc; i++) {
-        printf("Arg %d: '%s'\n", i, argv[i]);
-    }
-    char* endptr = "not set";
-    float pi     = strtof(argv[1], &endptr);
-    if (pi == 0) {
-        perror("strtod failed");
-        printf("Start: %p\n", argv[1]);
-        printf("Remaining: %p\n", endptr);
-    }
-    printf("%.2lf\n", pi);
+const char* pi_str = "3.14";
+
+int main() {
+    float pi = strtof(pi_str, NULL);
+    return (int)pi;
 }
