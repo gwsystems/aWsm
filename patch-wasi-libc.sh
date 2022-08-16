9b5df22065b01432f422d6705c61315887e5d5b9
Sean McBride <spmcbride1201@gmail.com>
2022-08-16 17:21:51 -0400

    chore: pin wasi-libc commit

--- patch-wasi-libc.sh
+++ patch-wasi-libc.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 git clone https://github.com/WebAssembly/wasi-libc.git
+pushd wasi-libc
+git reset --hard
+git clean -f
+git checkout 9886d3d6200fcc3726329966860fc058707406cd
+popd
 
 # Resize Wasm pages from 64k to 4k
 mv wasi-libc/libc-bottom-half/headers/public/__macro_PAGESIZE.h wasi-libc/libc-bottom-half/headers/public/__macro_PAGESIZE.h.old
 mv wasi-libc/expected/wasm32-wasi/predefined-macros.txt wasi-libc/expected/wasm32-wasi/predefined-macros.txt.old
 
-sed "s/#define PAGESIZE (0x10000)/#define PAGESIZE (0x1000)/" \
+sed "s/#define PAGESIZE (0x10000)/#define PAGESIZE (0x400)/" \
     wasi-libc/libc-bottom-half/headers/public/__macro_PAGESIZE.h.old \
     >wasi-libc/libc-bottom-half/headers/public/__macro_PAGESIZE.h
 
-sed "s/#define PAGESIZE (0x10000)/#define PAGESIZE (0x1000)/" \
+sed "s/#define PAGESIZE (0x10000)/#define PAGESIZE (0x400)/" \
     wasi-libc/expected/wasm32-wasi/predefined-macros.txt.old \
     >wasi-libc/expected/wasm32-wasi/predefined-macros.txt
 
@@ -18,9 +23,5 @@ rm wasi-libc/libc-bottom-half/headers/public/__macro_PAGESIZE.h.old
 rm wasi-libc/expected/wasm32-wasi/predefined-macros.txt.old
 
 make -C wasi-libc CC=/usr/bin/clang-13 \
-    AR=/usr/bin/llvm-ar-13 \
-    NM=/usr/bin/llvm-nm-13
-
-make CC=/usr/bin/clang-13 \
     AR=/usr/bin/llvm-ar-13 \
     NM=/usr/bin/llvm-nm-13
